753f5be164bb982ca7f62762fea2b6bf2524945f
vitanv <65992932+vitanv@users.noreply.github.com>
2022-12-09 18:53:00 +0000

    Time: 0 ms (100.00%), Space: 42.1 MB (84.67%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxAncestorDiff(TreeNode root) {
+        return dfs(root, root.val, root.val);
+    }
+
+    public int dfs(TreeNode root, int min, int max) {
+        if (root == null) return max - min;
+        max = Math.max(max, root.val);
+        min = Math.min(min, root.val);
+        return Math.max(dfs(root.left, min, max), dfs(root.right, min, max));
+    }
+}
