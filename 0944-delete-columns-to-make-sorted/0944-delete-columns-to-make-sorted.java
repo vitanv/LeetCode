61aa7a145baf250eacdc824429b79f746284d202
vitanv <65992932+vitanv@users.noreply.github.com>
2023-01-03 20:47:51 +0000

    Time: 17 ms (44.14%), Space: 47.4 MB (60.13%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        if(strs.length <= 1) return 0;
+        int answer = 0;
+        for(int i = 0; i < strs[0].length(); i++){
+            for(int j = 1; j < strs.length; j++){
+                if(strs[j].charAt(i) - strs[j-1].charAt(i) < 0){
+                    answer++;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
