49c8115965f0f9ab4c3d2bc5827ae837a5278474
vitanv <65992932+vitanv@users.noreply.github.com>
2023-01-08 20:52:19 +0000

    Time: 10 ms (98.86%), Space: 39.8 MB (96.95%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxPoints(int[][] points) {
+        if(points.length <= 2) return points.length;
+        int answer = 2;
+        
+        for(int i = 0; i < points.length; i++){
+            for(int j = i + 1; j < points.length; j++){
+                int temp = 2;
+                for(int k = j+1; k < points.length; k++){
+                     int x = (points[j][1] - points[i][1]) * (points[k][0] - points[i][0]);
+                    int y = (points[k][1] - points[i][1]) * (points[j][0] - points[i][0]);
+                    if(x == y){
+                        temp++;
+                    }
+                }
+                answer = Math.max(answer, temp);
+            }
+        }
+        return answer;
+    }
+}
