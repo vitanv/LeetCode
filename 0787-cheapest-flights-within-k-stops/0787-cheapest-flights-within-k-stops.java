095794f175f258274170f22116041a9f541ea7d0
vitanv <65992932+vitanv@users.noreply.github.com>
2023-01-26 23:50:09 +0000

    Time: 12 ms (35.54%), Space: 47.3 MB (20.86%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, List<int[]>> map = new HashMap<>();
+        for (int[] flight : flights)
+            map.computeIfAbsent(flight[0], value -> new ArrayList<>()).add(new int[] { flight[1], flight[2] });
+
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] { src, 0 });
+        int stops = 0;
+
+        while (stops <= k && !q.isEmpty()) {
+            int size = q.size();
+            while (size > 0) {
+                size--;
+                int[] temp = q.poll();
+                int node = temp[0];
+                int distance = temp[1];
+
+                if (!map.containsKey(node))
+                    continue;
+                // Loop over neighbors of popped node.
+                for (int[] e : map.get(node)) {
+                    int neighbour = e[0];
+                    int price = e[1];
+                    if (price + distance >= dist[neighbour])
+                        continue;
+                    dist[neighbour] = price + distance;
+                    q.offer(new int[] { neighbour, dist[neighbour] });
+                }
+            }
+            stops++;
+        }
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
+}
