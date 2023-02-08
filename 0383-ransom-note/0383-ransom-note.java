7065153882acdcdf8bcde2407675829bb211c376
vitanv <65992932+vitanv@users.noreply.github.com>
2023-02-08 03:37:50 +0000

    Time: 13 ms (54.26%), Space: 42.8 MB (54.58%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c: magazine.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        for(char c: ransomNote.toCharArray()){
+            if(!map.containsKey(c)) return false;
+            map.put(c, map.get(c)-1);
+            if (map.get(c) < 0) return false;
+        }
+        return true;
+    }
+}
