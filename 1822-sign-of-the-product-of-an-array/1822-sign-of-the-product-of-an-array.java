9832fa5e4a1e05c5dfdf7cacbb6feb3a6fed648a
vitanv <65992932+vitanv@users.noreply.github.com>
2023-05-02 02:28:52 +0100

    Time: 0 ms (100.00%), Space: 42.5 MB (28.63%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int answer = 1;
+        for(int num : nums){
+            if(num == 0) return 0;
+            if(num < 0) answer *= -1;
+        }
+        return answer;
+    }
+}
