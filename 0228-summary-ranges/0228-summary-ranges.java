f3d1f878a1937f4aa120534aee2bef33615236a9
vitanv <65992932+vitanv@users.noreply.github.com>
2023-06-12 22:30:50 +0100

    Time: 5 ms (84.18%), Space: 40.9 MB (57.57%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> answer = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            int start = nums[i];
+            while(i + 1 < nums.length && nums[i+1] - nums[i] == 1) i++;
+            if(start != nums[i]){
+                answer.add(""+start+"->"+nums[i]);
+            }else{
+                answer.add(""+start);
+            }
+        }
+        return answer;
+    }
+}
