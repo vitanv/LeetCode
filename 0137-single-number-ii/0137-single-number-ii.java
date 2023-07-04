3c4d066226c2a213d6208ff87c36f7b7a5402ccc
vitanv <65992932+vitanv@users.noreply.github.com>
2023-07-04 15:19:08 +0100

    Time: 6 ms (22.31%), Space: 43.6 MB (75.07%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for(int key: map.keySet()){
+            if(map.get(key) == 1) return key;
+        }
+        return 0;
+    }
+}
