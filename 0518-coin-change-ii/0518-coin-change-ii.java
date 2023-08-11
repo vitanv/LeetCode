0ba476629ef32dbdf00cbdd19d772afeefc59ab2
vitanv <65992932+vitanv@users.noreply.github.com>
2023-08-11 21:06:22 +0100

    Time: 7 ms (58.13%), Space: 45.5 MB (54.14%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp =new int[n + 1][amount + 1];
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++){
+            dp[i][0] = 1;
+            for(int j = 1; j <= amount; j++){
+                dp[i][j] = dp[i-1][j];
+                if(coins[i-1] <= j) dp[i][j] += dp[i][j-coins[i-1]];
+            }
+        }
+        return dp[n][amount];
+    }
+}
