d4c5f6b3af9bc0ecba51f39077238935317a8702
vitanv <65992932+vitanv@users.noreply.github.com>
2023-09-29 20:26:55 +0100

    Time: 2 ms (62.86%), Space: 55.9 MB (40.74%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean increasing = true, decreasing = true;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] > nums[i-1]) decreasing = false;
+            if(nums[i] < nums[i-1]) increasing = false;
+        }
+        return increasing || decreasing;
+    }
+}
