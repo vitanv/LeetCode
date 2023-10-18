d852f0754a5ead66a07a1ccba1553647acab1a1e
vitanv <65992932+vitanv@users.noreply.github.com>
2023-10-18 23:28:17 +0100

    Time: 0 ms, Memory: 41.1 MB - LeetHub

--- remove-element/remove-element.java
+++ remove-element/remove-element.java
@@ -1,10 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int index = 0;
-        for(int i = 0; i < nums.length;i++){
-            nums[index] = nums[i];
-            if(nums[index] != val) index++;
+        int answer = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != val) {
+                nums[answer] = nums[i];
+                answer++;
+            }
         }
-        return index;
+        return answer;
     }
 }
