e39d080b25718d10bde191ecc69c6c6dcae88f83
vitanv <65992932+vitanv@users.noreply.github.com>
2023-10-28 03:38:08 +0100

    Time: 17 ms (51.82%), Space: 43.5 MB (51.49%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countVowelPermutation(int n) {
+        int mod = (int) (1e9 + 7);
+        long[][] dp = new long[n + 1][5];
+        for (int i = 0; i < 5; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            dp[i+1][0] = (dp[i][4] + dp[i][1] + dp[i][2]) % mod;
+            dp[i+1][1] = (dp[i][0] + dp[i][2]) % mod;
+            dp[i+1][2] = (dp[i][3] + dp[i][1]) % mod;
+            dp[i+1][3] = (dp[i][2]) % mod;
+            dp[i+1][4] = (dp[i][2] + dp[i][3]) % mod;
+        }
+
+        long answer = 0;
+        for (int i = 0; i < 5; i++)
+            answer = (answer + dp[n][i]) % mod;
+        return (int) answer;
+    }
+}
