4ba0145ad4db9af778f4db4329afd43394bfb619
vitanv <65992932+vitanv@users.noreply.github.com>
2023-10-31 20:11:10 +0000

    Time: 1 ms (100.00%), Space: 58.3 MB (83.35%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] findArray(int[] pref) {
+        int n = pref.length;
+        int[] answer = new int[n];
+        answer[0] = pref[0];
+        for(int i = 1; i < n; i ++){
+            answer[i] = pref[i] ^ pref[i - 1];
+        }
+        return answer;
+    }
+}
