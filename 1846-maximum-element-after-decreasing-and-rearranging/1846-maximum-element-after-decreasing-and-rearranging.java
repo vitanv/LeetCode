7c2a0ae6d7fc80535ab7e79e83573065027bf292
vitanv <65992932+vitanv@users.noreply.github.com>
2023-11-15 11:56:42 +0000

    Time: 6 ms (82.14%), Space: 57.2 MB (10.71%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        if(arr.length == 1) return 1;
+        Arrays.sort(arr);
+        if(arr[0] != 1) arr[0] = 1;
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] - arr[i - 1] > 1) arr[i] = arr[i - 1] + 1;
+        }
+        return arr[arr.length - 1];
+    }
+}
