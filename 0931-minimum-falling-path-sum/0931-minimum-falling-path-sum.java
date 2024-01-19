af60f63cc2fdeedc0d49066966ac0e4bbe7b5426
vitanv <65992932+vitanv@users.noreply.github.com>
2024-01-19 01:00:26 +0000

    Time: 6 ms (68.64%), Space: 45.1 MB (41.66%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,20 +1,20 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int len = matrix.length;
+        int n = matrix.length;
         int answer = Integer.MAX_VALUE;
-        for(int i = 1; i < len; i++){
-            for(int j = 0; j < len; j++){
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < n; j++){
                 if(j == 0) {
                     matrix[i][j] = Math.min(matrix[i-1][j], matrix[i-1][j+1]) + matrix[i][j];
-                }else if(j == len-1){
+                }else if(j == n-1){
                     matrix[i][j] = Math.min(matrix[i-1][j], matrix[i-1][j-1]) + matrix[i][j];
                 }else{
                      matrix[i][j] = Math.min(matrix[i-1][j],Math.min(matrix[i-1][j-1],matrix[i-1][j+1])) + matrix[i][j];
                 }
             }
         }
-        for(int i = 0; i < len; i++){
-            answer = Math.min(answer, matrix[len-1][i]);
+        for(int i = 0; i < n; i++){
+            answer = Math.min(answer, matrix[n-1][i]);
         }
         return answer;
     }
