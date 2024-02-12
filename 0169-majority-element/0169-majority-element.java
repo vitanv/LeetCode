76dbd9aef640dc6c16c83f5ad21a78ec29d38ba9
vitanv <65992932+vitanv@users.noreply.github.com>
2024-02-12 09:56:31 +0000

    Time: 13 ms (27.64%), Space: 48.9 MB (77.86%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for(int key: map.keySet()){
+            if(map.get(key) > nums.length/2) return key;
+        }
+        return 0;
+    }
+}
