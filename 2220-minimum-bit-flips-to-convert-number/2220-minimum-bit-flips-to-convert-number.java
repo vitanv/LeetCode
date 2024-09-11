cccab8418e486d74fc4343cd92b12818e82a1324
vitanv <65992932+vitanv@users.noreply.github.com>
2024-09-11 01:19:35 +0100

    Time: 0 ms (100%), Space: 40.3 MB (64.36%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int answer = 0;
+        while(start > 0 || goal > 0){
+            if((start & 1) != (goal & 1)) answer++;
+            start >>= 1;
+            goal >>= 1;
+        }
+        return answer;
+    }
+}
