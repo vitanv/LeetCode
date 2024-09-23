72863e3d6cf550ce50be028ec582a311dbbc6c7f
vitanv <65992932+vitanv@users.noreply.github.com>
2024-09-23 17:10:41 +0100

    Time: 55 ms (14.63%), Space: 45.4 MB (36.39%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -7,9 +7,16 @@ class Solution {
             dp[i] = dp[i + 1] + 1;
             for(int j = i; j < n; j ++){
                 String substr = s.substring(i, j + 1);
-                if(set.contains(substr)) dp[i] = Math.min(dp[i], dp[j + 1]);
+                if(set.contains(substr)) {
+                    if(dp[i] < dp[j+1]) {
+                        System.out.println(s.charAt(i));
+                        System.out.println(s.charAt(j+1));
+                    }
+                    dp[i] = Math.min(dp[i], dp[j + 1]);
+                }
             }
         }
+        System.out.println(Arrays.toString(dp));
         return dp[0];
     }
 }
