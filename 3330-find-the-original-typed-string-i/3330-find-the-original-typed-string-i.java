ebe8c3d4791f7d5aed503f4ba5d6a462744abad0
vitanv <65992932+vitanv@users.noreply.github.com>
2025-07-01 15:21:48 +0100

    Time: 1 ms (87.19%), Space: 42.1 MB (61.16%) - LeetHub

---
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        int answer = 1;
+        for(int i = 1; i < word.length(); i++){
+            if(word.charAt(i - 1) == word.charAt(i)) answer++;
+        }
+        return answer;
+    }
+}
