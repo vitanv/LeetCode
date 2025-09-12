9c690f68bcb6d6a8505fc90a51127e44197f8a02
vitanv <65992932+vitanv@users.noreply.github.com>
2025-09-12 19:37:42 +0100

    Time: 4 ms (79.79%), Space: 45.3 MB (96.59%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for(char c: s.toCharArray()){
+            if(c == 'a' || c == 'e'|| c == 'i'|| c == 'o'|| c == 'u' ) return true;
+        }
+        return false;
+    }
+}
