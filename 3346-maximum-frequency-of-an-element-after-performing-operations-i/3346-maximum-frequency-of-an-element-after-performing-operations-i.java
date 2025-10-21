1c8e2e7950a8522aa6b4de900c7b2ae378098739
vitanv <65992932+vitanv@users.noreply.github.com>
2025-10-21 01:13:57 +0100

    Time: 85 ms (65.79%), Space: 64.4 MB (28.29%) - LeetHub

---
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k, int numOperations) {
+        int n = nums.length, answer = 0, left = 0, right = 0;
+        Arrays.sort(nums);
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for(int num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
+        for(int mid = 0; mid < n; mid++) {
+            while(nums[mid] - nums[left] > k) {
+                left++;
+            }
+            while(right < n - 1 && nums[right + 1] - nums[mid] <= k) {
+                right++;
+            }
+            int total = right - left + 1; 
+            answer = Math.max(answer, Math.min(total - count.get(nums[mid]), numOperations) + count.get(nums[mid]));
+        }
+        left = 0;
+        for(right = 0; right < n; right++) {
+            int mid = (nums[left] + nums[right]) / 2;
+            while(mid - nums[left] > k || nums[right] - mid > k) {
+                left++;
+                mid = (nums[left] + nums[right]) / 2;
+            }
+            answer = Math.max(answer, Math.min(right - left + 1, numOperations));
+        }
+
+        return answer;
+    }
+}
