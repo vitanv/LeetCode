809c12f52c3b2c1637fcb115237df5cd8700ee29
vitanv <65992932+vitanv@users.noreply.github.com>
2025-12-05 02:38:37 +0000

    Time: 0 ms (100%), Space: 43.3 MB (44.12%) - LeetHub

---
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countPartitions(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum % 2 == 0 ? nums.length - 1 : 0;
+    }
+}
