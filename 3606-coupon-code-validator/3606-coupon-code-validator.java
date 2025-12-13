7c4a9b78784bedfc2192322945d91260e58ea373
vitanv <65992932+vitanv@users.noreply.github.com>
2025-12-13 20:01:17 +0000

    Time: 7 ms (88.44%), Space: 47.5 MB (50.22%) - LeetHub

---
+++ 3606-coupon-code-validator/3606-coupon-code-validator.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+        Map<String, Integer> priority = new HashMap<>();
+        priority.put("electronics", 0);
+        priority.put("grocery", 1);
+        priority.put("pharmacy", 2);
+        priority.put("restaurant", 3);
+
+        List<Pair> valid = new ArrayList<>();
+
+        for (int i = 0; i < code.length; i++) {
+            if (isActive[i] && priority.containsKey(businessLine[i]) && isValidCode(code[i])) {
+                valid.add(new Pair(priority.get(businessLine[i]), code[i]));
+            }
+        }
+        Collections.sort(valid, (a, b) -> {
+            if (a.priority != b.priority)
+                return a.priority - b.priority;
+            return a.code.compareTo(b.code);
+        });
+
+        List<String> answer = new ArrayList<>();
+        for (Pair p : valid) {
+            answer.add(p.code);
+        }
+
+        return answer;
+    }
+
+    private boolean isValidCode(String s) {
+        if (s.length() == 0) return false;
+        for (char c : s.toCharArray()) {
+            if (!Character.isLetterOrDigit(c) && c != '_')
+                return false;
+        }
+        return true;
+    }
+    static class Pair {
+        int priority;
+        String code;
+
+        Pair(int priority, String code) {
+            this.priority = priority;
+            this.code = code;
+        }
+    }
+}
