d00422e2407a48c43bf928fca2ee2dd32b17c716
vitanv <65992932+vitanv@users.noreply.github.com>
2025-12-15 11:48:55 +0000

    Time: 2 ms (100%), Space: 84.3 MB (31.85%) - LeetHub

---
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long answer = 1, count = 1;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i - 1] == prices[i] + 1){
+                count++;
+            }else{
+                count=1;
+            }
+            answer+= count;
+        }
+        return answer;
+    }
+}
