8c0f4efa83ac0143b2e5ccfa519fc6a049037579
vitanv <65992932+vitanv@users.noreply.github.com>
2026-05-03 02:16:15 +0100

    Time: 0 ms (100%), Space: 42.8 MB (58.69%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+        String doubleS = s + s;
+        return doubleS.contains(goal);
+    }
+}
