ca48d1eaa7265e0e3431f27ebd0b791bc5269806
vitanv <65992932+vitanv@users.noreply.github.com>
2026-07-07 23:01:28 +0100

    Time: 1 ms (99.85%), Space: 42.4 MB (91.2%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long sumAndMultiply(int n) {
+        long x = 0, sum = 0,pow10 = 1;
+        while (n > 0) {
+            int d = n % 10;
+            sum += d;
+            if (d > 0) {
+                x += d * pow10;
+                pow10 *= 10;
+            }
+            n /= 10;
+        }
+        return x * sum;
+    }
+}
